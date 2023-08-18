6f47de302e24ca32d31fd8c3f06defe8a21181d2
maximizzar <60427093+maximizzar@users.noreply.github.com>
2023-08-18 06:23:02 +0200

    refactor: Ordered sort function calls in main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,27 @@
-
 fn main() {
-    let mut array = vec![5,6,4,3,6,8,5,5,8,4,2,6,9,4,3,1,0,1,0,0,4,5,8,7,3,2,4,5,3,2,1,5];
+    let mut master_array = vec![5,6,4,3,6,8,5,5,8,4,2,6,9,4,3,1,0,1,0,0,4,5,8,7,3,2,4,5,3,2,1,5];
+    {
+        let array = master_array.clone();
+        let start = std::time::Instant::now();
+        bubble_sort(array);
+        let duration = start.elapsed();
+        println!("Time elapsed in bubble_sort() is: {:?}", duration);
+    }
+    {
+        let array = master_array.clone();
 
-    let mut start = std::time::Instant::now();
-    bubble_sort(array);
-    let mut duration = start.elapsed();
-    println!("Time elapsed in bubble_sort() is: {:?}", duration);
-
-    array = vec![5,6,4,3,6,8,5,5,8,4,2,6,9,4,3,1,0,1,0,0,4,5,8,7,3,2,4,5,3,2,1,5];
-
-    start = std::time::Instant::now();
-    insertion_sort(array);
-    duration = start.elapsed();
-    println!("Time elapsed in insertion_sort() is: {:?}", duration);
-
-    array = vec![5,6,4,3,6,8,5,5,8,4,2,6,9,4,3,1,0,1,0,0,4,5,8,7,3,2,4,5,3,2,1,5];
-
-    start = std::time::Instant::now();
-    array = counting_sort(array.to_owned(), 10);
-    duration = start.elapsed();
-    println!("Time elapsed in counting_sort() is: {:?}", duration);
-    println!("{:?}", array)
+        let start = std::time::Instant::now();
+        counting_sort(array.to_owned(), 10);
+        let duration = start.elapsed();
+        println!("Time elapsed in counting_sort() is: {:?}", duration);
+    }
+    {
+        let array = master_array.clone();
+        let start = std::time::Instant::now();
+        insertion_sort(array);
+        let duration = start.elapsed();
+        println!("Time elapsed in insertion_sort() is: {:?}", duration);
+    }
 }
 
 fn bubble_sort(mut array: Vec<i64>) -> Vec<i64> {
@@ -39,19 +40,6 @@ fn bubble_sort(mut array: Vec<i64>) -> Vec<i64> {
     return array;
 }
 
-fn insertion_sort(mut array: Vec<i64>) -> Vec<i64> {
-    for j in 2..array.len() {
-        let key = array[j];
-        let mut i = j - 1;
-
-        while i > 0 && array[i] > key {
-            array[i + 1] = array[i];
-            i -= 1;
-        }
-        array[i + 1] = key;
-    }
-    return array;
-}
 fn counting_sort(array: Vec<i64>, biggest_number: usize) -> Vec<i64> {
     let mut c: Vec<i64> = vec![0; biggest_number + 1];
     let mut output: Vec<i64> = vec![0; array.len()];
@@ -70,4 +58,17 @@ fn counting_sort(array: Vec<i64>, biggest_number: usize) -> Vec<i64> {
         c[array[i] as usize] -= 1;
     }
     return output;
+}
+fn insertion_sort(mut array: Vec<i64>) -> Vec<i64> {
+    for j in 2..array.len() {
+        let key = array[j];
+        let mut i = j - 1;
+
+        while i > 0 && array[i] > key {
+            array[i + 1] = array[i];
+            i -= 1;
+        }
+        array[i + 1] = key;
+    }
+    return array;
 }
