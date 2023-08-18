1a5408bd31e953b0ef1ee91e72a1d0a749face5b
maximizzar <60427093+maximizzar@users.noreply.github.com>
2023-08-18 23:42:28 +0200

    fix: counting_sort and insertion_sort + new workflow

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,34 @@
+
 fn main() {
-    let mut master_array = vec![5,6,4,3,6,8,5,5,8,4,2,6,9,4,3,1,0,1,0,0,4,5,8,7,3,2,4,5,3,2,1,5];
+    const LARGEST_VALUE: usize = 10000;
+    let master_vector: Vec<i64> = vec![7138, 5993, 1106, 4619, 1821, 7534, 7863];
     {
-        let array = master_array.clone();
+        let vector= master_vector.clone();
         let start = std::time::Instant::now();
-        bubble_sort(array);
+        bubble_sort(vector);
         let duration = start.elapsed();
         println!("Time elapsed in bubble_sort() is: {:?}", duration);
     }
     {
-        let array = master_array.clone();
-
+        let vector= master_vector.clone();
         let start = std::time::Instant::now();
-        counting_sort(array.to_owned(), 10);
+        counting_sort(vector.to_owned(),LARGEST_VALUE);
         let duration = start.elapsed();
         println!("Time elapsed in counting_sort() is: {:?}", duration);
     }
     {
-        let array = master_array.clone();
+        let mut vector = master_vector.clone();
         let start = std::time::Instant::now();
-        insertion_sort(array);
+        insertion_sort(&mut vector);
         let duration = start.elapsed();
         println!("Time elapsed in insertion_sort() is: {:?}", duration);
+    }
+    {
+        let mut vector = master_vector.clone();
+        let start = std::time::Instant::now();
+        radix_sort(&mut vector,LARGEST_VALUE);
+        let duration = start.elapsed();
+        println!("Time elapsed in radix_sort_sort() is: {:?}", duration);
     }
 }
 
@@ -28,10 +36,10 @@ fn bubble_sort(mut array: Vec<i64>) -> Vec<i64> {
     for _ in 0..array.len() {
         let mut sorted = true;
         for i in 0..array.len() - 1 {
-           if array[i] > array[i + 1] {
-               sorted = false;
-               array.swap(i,i + 1);
-           }
+            if array[i] > array[i + 1] {
+                sorted = false;
+                array.swap(i,i + 1);
+            }
         }
         if sorted {
             break;
@@ -40,8 +48,8 @@ fn bubble_sort(mut array: Vec<i64>) -> Vec<i64> {
     return array;
 }
 
-fn counting_sort(array: Vec<i64>, biggest_number: usize) -> Vec<i64> {
-    let mut c: Vec<i64> = vec![0; biggest_number + 1];
+fn counting_sort(array: Vec<i64>, largest_value: usize) -> Vec<i64> {
+    let mut c: Vec<i64> = vec![0; largest_value + 1];
     let mut output: Vec<i64> = vec![0; array.len()];
 
     //count numbers in array
@@ -50,7 +58,7 @@ fn counting_sort(array: Vec<i64>, biggest_number: usize) -> Vec<i64> {
     }
 
     //accumulate numbers in c
-    for i in 1..biggest_number {
+    for i in 1..largest_value {
         c[i] = c[i - 1] + c[i];
     }
     for i in 0..array.len() {
@@ -59,16 +67,47 @@ fn counting_sort(array: Vec<i64>, biggest_number: usize) -> Vec<i64> {
     }
     return output;
 }
-fn insertion_sort(mut array: Vec<i64>) -> Vec<i64> {
-    for j in 2..array.len() {
-        let key = array[j];
-        let mut i = j - 1;
 
-        while i > 0 && array[i] > key {
-            array[i + 1] = array[i];
-            i -= 1;
+fn insertion_sort(vector: &mut Vec<i64>) -> Vec<i64> {
+    for i in 0..vector.len() - 2 {
+        let mut i_next = i + 1;
+        let n_current = vector[i_next];
+
+        while i_next > 0 && n_current < vector[i_next - 1] {
+            vector[i_next] = vector[i_next - 1];
+            i_next -= 1;
         }
-        array[i + 1] = key;
+        vector[i_next] = n_current;
     }
-    return array;
+    return vector.to_owned();
+}
+
+fn radix_sort(vector: &mut Vec<i64>, largest_value: usize) -> Vec<i64> {
+    let largest_power_of_ten = largest_value - largest_value % 10;
+    let largest_power: i64 = (f64::log(largest_power_of_ten as f64, 10.0) as i64) + 1;
+
+    for current_power in 0..largest_power as usize {
+        let mut bucket_vector: Vec<Vec<i64>> = vec![vec![]; 10];
+
+        // put input_vector in buckets
+        for i in 0..vector.len() {
+            let i_bucket_vector: usize = (vector[i] as f64 /
+                f64::powf(10.,current_power as f64)) as usize % 10;
+
+            bucket_vector[i_bucket_vector].push(vector[i]);
+        }
+
+        //fill input_vector from buckets
+        if current_power == (largest_power - 1) as usize {
+            vector.copy_from_slice(&*bucket_vector[0]);
+        } else {
+            vector.clear();
+            for c in 0..10 {
+                for r in 0..bucket_vector[c].len() {
+                    vector.push(bucket_vector[c][r]);
+                }
+            }
+        }
+    }
+    return vector.to_owned();
 }
