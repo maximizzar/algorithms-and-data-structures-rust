93ca6a5a1e66f811342b74534532f165329df572
maximizzar <60427093+maximizzar@users.noreply.github.com>
2023-08-23 22:52:36 +0200

    fix: removed error prone code

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,3 @@
-use std::cmp::min;
-use std::mem::swap;
 
 fn main() {
     const LARGEST_VALUE: usize = 10000;
@@ -24,13 +22,6 @@ fn main() {
         insertion_sort(&mut vector);
         let duration = start.elapsed();
         println!("Time elapsed in insertion_sort() is: {:?}", duration);
-    }
-    {
-        let mut vector = master_vector.clone();
-        let start = std::time::Instant::now();
-        //println!("{:?}", merge_sort(&mut vector));
-        let duration = start.elapsed();
-        //println!("Time elapsed in merge_sort() is: {:?}", duration);
     }
     {
         let mut vector = master_vector.clone();
@@ -98,56 +89,6 @@ fn insertion_sort(vector: &mut Vec<i64>) -> Vec<i64> {
     return vector.to_owned();
 }
 
-fn merge_sort(mut vector: &mut Vec<i64>) -> Vec<i64> {
-    let n: usize = vector.len();
-    let mut buffer: Vec<i64> = vec![0; n];
-    /*
-        "run" splits the vector vector into n number of sub-vectors with the size of run_width.
-        Those sub-vectors getting sorted into the buffer vector.
-        When vector with current "run_width" is sorted into buffer
-        the buffer will replace the vector "vector".
-    */
-    for mut width in 1..n {
-
-        // vector "vector" is full of runs of length run_width.
-        for mut index_left in 0..n {
-            let index_right = std::cmp::min(index_left + width, n);
-            let index_end = std::cmp::min(index_left + 2 * width, n);
-
-            // Merge two runs: A[i:i+width-1] and A[i+width:i+2*width-1] to B[]
-            // or copy A[i:n-1] to B[] ( if (i+width >= n) )
-            bottom_up_merge(vector, index_left, index_right, index_end, buffer.to_owned());
-            index_left += 2 * width;
-        }
-        // Now work array B is full of runs of length 2*width.
-        // Copy array B to array A for the next iteration.
-        // A more efficient implementation would swap the roles of A and B.
-        vector.copy_from_slice(&*buffer);
-        // Now array A is full of runs of length 2*width.
-        width *= 2;
-    }
-    drop(buffer);
-    return vector.to_owned();
-
-    //  Left run is A[iLeft :iRight-1].
-    // Right run is A[iRight:iEnd-1  ].
-    fn bottom_up_merge(vector: &Vec<i64>, index_left: usize, index_right: usize, index_end: usize, mut buffer: Vec<i64>) {
-        let mut i: usize = index_left; let mut j: usize = index_right;
-        buffer.clear();
-        // While there are elements in the left or right runs...
-        for k in index_left..index_end {
-            // If left run head exists and is <= existing right run head.
-            if i < index_right && (j >= index_end || vector[i] <= vector[j]) {
-                buffer.push(vector[i]);
-                i += 1;
-            } else {
-                buffer.push(vector[j]);
-                j += 1;
-            }
-        }
-    }
-}
-
 fn radix_sort(vector: &mut Vec<i64>, largest_value: usize) -> Vec<i64> {
     let largest_power_of_ten = largest_value - largest_value % 10;
     let largest_power: i64 = (f64::log(largest_power_of_ten as f64, 10.0) as i64) + 1;
@@ -178,7 +119,7 @@ fn radix_sort(vector: &mut Vec<i64>, largest_value: usize) -> Vec<i64> {
     return vector.to_owned();
 }
 
-fn selection_sort(mut vector: &mut Vec<i64>) -> &mut Vec<i64> {
+fn selection_sort(vector: &mut Vec<i64>) -> &mut Vec<i64> {
     let highest_index = vector.len();
     let mut insertion_index: usize = 0;
 
